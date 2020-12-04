5e2603bd47904e33431555f0e39b1893b2da495f
Meron Ogbai <okbaymeron@gmail.com>
2020-12-04 11:10:21 +0300

    Add book constructor function

---
+++ main.js
@@ -0,0 +1,9 @@
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = function() {
+    return `${title} by ${author}, ${pages} pages, ${read}`
+  }
+}
