ea1bf8acd68280cf07f9799c78b81edde3cbb8fa
Meron Ogbai <okbaymeron@gmail.com>
2020-12-04 13:59:43 +0300

    Finish add book to form functionality

--- main.js
+++ main.js
@@ -12,8 +12,6 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-addBookToLibrary(new Book)
-
 let bookOne = new Book("Book One", "JKR", 53, "yes");
 let bookTwo = new Book("Book Two", "Author Two", 23, "no");
 let bookThree = new Book("Book Three", "Author Three", 103, "yes");
@@ -21,27 +19,27 @@ let bookThree = new Book("Book Three", "Author Three", 103, "yes");
 let myLibrary = [bookOne, bookTwo, bookThree];
 
 function displayBook(book) {
-    const table = document.querySelector('#table')
-    const tr = document.createElement('tr')
-    const title = document.createElement('td')
-    const author = document.createElement('td')
-    const pages = document.createElement('td')
-    const read = document.createElement('td')
+  const table = document.querySelector('#table')
+  const tr = document.createElement('tr')
+  const title = document.createElement('td')
+  const author = document.createElement('td')
+  const pages = document.createElement('td')
+  const read = document.createElement('td')
 
-    title.textContent = book.title
-    author.textContent = book.author
-    pages.textContent = book.pages
-    read.textContent = book.read
-    
-    tr.appendChild(title)
-    tr.appendChild(author)
-    tr.appendChild(pages)
-    tr.appendChild(read)
-    table.appendChild(tr)
+  title.textContent = book.title
+  author.textContent = book.author
+  pages.textContent = book.pages
+  read.textContent = book.read
+
+  tr.appendChild(title)
+  tr.appendChild(author)
+  tr.appendChild(pages)
+  tr.appendChild(read)
+  table.appendChild(tr)
 }
 
 function eachBook(library) {
-  for(let i = 0; i < library.length; i++) {
+  for (let i = 0; i < library.length; i++) {
     displayBook(library[i])
   }
 }
@@ -52,9 +50,21 @@ function displayForm() {
   document.getElementById("newBookForm").style.display = "block";
 }
 
-function getFormData() {
-  let form = document.forms.newBookForm
+newBookForm = document.forms.newBookForm
+newBookForm.addEventListener('submit', function (e) {
+  e.preventDefault()
+  const title = newBookForm.querySelector('input[name="title"').value
+  const author = newBookForm.querySelector('input[name="author"').value
+  const pages = newBookForm.querySelector('input[name="pages"').value
+  const possibleReadValues = newBookForm.querySelectorAll('input[name="read"]')
+  let read
+  if (possibleReadValues[0].checked) {
+    read = 'Yes'
+  } else {
+    read = 'No'
+  }
 
-  let formData = new FormData(form)
-  let formTitle = formData.get('title')
-}
+  book = new Book(title, author, pages, read)
+  displayBook(book)
+  addBookToLibrary(book)
+})
